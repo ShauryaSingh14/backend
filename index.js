b021deb25df93dbb26612c33aa60e50bc73d8ae8
CodeSandbox <support@codesandbox.io>
2024-06-25 05:29:09 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello CodeSandbox");
+require("dotenv").config();
+
+const express = require("express");
+// import express from "express" another way
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/home", (req, res) => {
+  res.send("<h1>Login into chai aur code </h1>");
+});
+
+app.get("/chai", (res, req) => {
+  res.send("I love chai");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening at port ${port}`);
+});
